f127b68753e0fc6e4c1ad0602a0c052c3d428fcf
Donald Clark Jackson <dcj@clark-communications.com>
2012-06-08 17:55:14 -0700

    Got it to work

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-facebook-graph "0.4.0"
+(defproject net.likestream/clj-facebook-graph "0.4.0"
   :description "A Clojure client for the Facebook Graph API."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"  :exclude [org.clojure/clojure]]
@@ -7,9 +7,9 @@
                  [clj-http "0.3.6"]
                  [clj-oauth2 "0.2.0"]]
 
-  :dev-dependencies [[ring/ring-devel "1.0.1"  :exclude [org.clojure/clojure]]
-                     [ring/ring-jetty-adapter "1.0.1"  :exclude [org.clojure/clojure]]
-                     [compojure "1.0.1"  :exclude [org.clojure/clojure]]]
+  ;; :dev-dependencies [[ring/ring-devel "1.0.1"  :exclude [org.clojure/clojure]]
+  ;;                    [ring/ring-jetty-adapter "1.0.1"  :exclude [org.clojure/clojure]]
+  ;;                    [compojure "1.0.1"  :exclude [org.clojure/clojure]]]
 
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"}
 
